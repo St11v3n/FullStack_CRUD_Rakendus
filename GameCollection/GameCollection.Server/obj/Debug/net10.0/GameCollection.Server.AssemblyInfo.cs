04e4613926db586b23b18f9864fa98b32b154098
St11v3n <stiiven111@gmail.com>
2026-06-08 19:31:48 +0300

    additional commit

--- GameCollection/GameCollection.Server/obj/Debug/net10.0/GameCollection.Server.AssemblyInfo.cs
+++ GameCollection/GameCollection.Server/obj/Debug/net10.0/GameCollection.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameCollection.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2cf9c26cd681841505697b1df7dd185f078c850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6142daeacba64fde8b053b4c4896c039645afea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameCollection.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameCollection.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
